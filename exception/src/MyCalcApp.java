657722a7cb7bad92ebb6a19b7b3cecc425048e72
ravinderkabil <kabli.ravinder@gmail.com>
2025-07-02 12:15:01 +0530

    Committed changes for Calculator App.

--- exception/src/MyCalcApp.java
+++ exception/src/MyCalcApp.java
@@ -1,18 +1,20 @@
+import java.util.Scanner;
+
 /**
  * This class accepts a number from the user and passes that to calDouble() method
  */
 public class MyCalcApp {
     public static void main(String[] args) {
-        if(args != null && args.length>0){
-            try{
-                int num = Integer.parseInt(args[0]);
-                Calculator calculator = new Calculator();
-                double doubleValue = calculator.calDouble(num);
-                System.out.println("Double value returned is " + doubleValue);
-            }catch(MyArithException maex){
-                System.out.println("Number less than or equal to zero encountered" + maex.getMessage());
-            }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
 
+        try {
+            Calculator calculator = new Calculator();
+            double doubleValue = calculator.calDouble(number);
+            System.out.println("Double value returned is " + doubleValue);
+        } catch (MyArithException maex) {
+            System.out.println("Number less than or equal to zero encountered" + maex.getMessage());
         }
     }
 }
