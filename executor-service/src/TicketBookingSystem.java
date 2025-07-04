06428bc900897f92f65a9e6478ce919ce64ac28b
ravinderkabil <kabli.ravinder@gmail.com>
2025-07-04 17:01:08 +0530

    Committed changes for Executor service

--- executor-service/src/TicketBookingSystem.java
+++ executor-service/src/TicketBookingSystem.java
@@ -4,13 +4,10 @@ import java.util.concurrent.Executors;
 public class TicketBookingSystem {
     public static void main(String[] args) {
         int numberOfBookings = 5;
-        try (ExecutorService executorService = Executors.newFixedThreadPool(3)) {
-
-            for (int i = 0; i < numberOfBookings; i++) {
-                executorService.execute(new TicketBookingTask(i + 1));
-            }
-            executorService.shutdown();
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        for (int i = 0; i < numberOfBookings; i++) {
+            executorService.execute(new TicketBookingTask(i + 1));
         }
-
+        executorService.shutdown();
     }
 }
